862ea05cd3490ae2674bf9f566b203c46a09f221
alanmrj01 <158050238+alanmrj01@users.noreply.github.com>
2026-08-08 17:23:58 +0000

    up bug

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-{
-  "files": [],
-  "references": [
-    { "path": "./tsconfig.app.json" },
-    { "path": "./tsconfig.node.json" }
-  ]
-}
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true,
+    port: 5173,
+  },
+})
